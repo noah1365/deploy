50e2c2766050735e4188a48d588f6437d3bac9bf
JL1365 <borlagdatanjohnlloyd0@gmail.com>
2024-09-24 21:40:08 +0800

    try deploy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 
@@ -18,6 +19,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/auth",benefitRoute);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT,() => {
     connectDB();
     console.log(`Server is running PORT: ${PORT}`);
--- frontend/src/store/benefitStore.js
+++ frontend/src/store/benefitStore.js
@@ -1,7 +1,11 @@
 import { create } from 'zustand';
 import axios from 'axios';
 
-const API_URL = "http://localhost:7687/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+
+// const API_URL = process.env.NODE_ENV === "production" 
+//   ? "https://backend-hr3.jjm-manufacturing.com/api/auth" 
+//   : "http://localhost:7687/api/auth";
 
 axios.defaults.withCredentials = true;
 
