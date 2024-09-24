a7db17cbbdce30cf8156ae3973f96f9f657d16da
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2024-09-24 22:53:18 +0800

    dadadadadadadafs sfsfssffvsvfsfvsfvsfsfvsvfsfvssfsvfsvfsfvsfsfsssfssfsfssfsfdsfssdfssdsfsffs

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,12 @@ app.use("/api/auth",authRoute);
 app.use("/api/auth",benefitRoute);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
+
 
 app.listen(PORT,() => {
     connectDB();
